7423dae0073a160374e90d2338d764fd2cde8074
alangabriel45 <139440268+alangabriel45@users.noreply.github.com>
2024-04-06 21:05:31 +0800

    Done Change navbar login logout

---
+++ TropicTrail/TropicTrail/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace TropicTrail.Controllers
+{
+    [Authorize]
+    public class HomeController : BaseController
+    {
+        // GET: Home
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            var user = _userRepo.Table().Where(m => m.userName == u.userName).FirstOrDefault();
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(u.userName, false);
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "User does not exist or incorrect password!");
+
+            return View(u);
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+        [AllowAnonymous]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Create(User u)
+        {
+            _userRepo.Create(u);
+            TempData["Msg"] = $"User {u.userName} Added!";
+
+            return RedirectToAction("Login");
+        }
+        public ActionResult HotDeals()
+        {
+            return View();
+        }
+        public ActionResult Offers()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
